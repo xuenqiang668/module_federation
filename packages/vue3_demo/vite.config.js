70c258fca90e4e847c806806e800ff9336b51a4f
xun8szh_t <external.enqiang.xu@cn.bosch.com>
2025-01-10 16:21:01 +0800

    pref: pref

--- packages/vue3_demo/vite.config.js
+++ packages/vue3_demo/vite.config.js
@@ -20,6 +20,9 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+  preview: {
+    port: 4000
+  },
   build: {
     target: 'es2022'
   }
--- packages/vue3_demo2/vite.config.js
+++ packages/vue3_demo2/vite.config.js
@@ -4,15 +4,7 @@ import federation from "@originjs/vite-plugin-federation";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), , federation({
-    name: 'remote-app',
-    filename: 'remoteEntry.js',
-    // Modules to expose
-    exposes: {
-      './Button': './src/components/Button.vue',
-    },
-    shared: ['vue']
-  })],
+  plugins: [vue()],
   base: "http://localhost:3003",
   server: {
     port: 3002,
